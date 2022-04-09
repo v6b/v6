e0855fea10015da781ff4df39a60b7bad538f1ad
v6b <99478294+v6b@users.noreply.github.com>
2022-04-09 12:58:07 +0000

    Update

--- warp_unlock/unlock.sh
+++ warp_unlock/unlock.sh
@@ -182,8 +182,11 @@ check_warp(){
 		
 		# 在已安装 Client 的前提下，区分模式 Mode
 		if type -P warp-cli >/dev/null 2>&1; then
-			if [[ -e /etc/wireguard/luban ]]; then
-				if [[ ! $(ip a) =~ 'CloudflareWARP' ]]; then
+			if [[ $(warp-cli --accept-tos settings) =~ WarpProxy ]]; then
+				[[ ! $(ss -nltp) =~ 'warp-svc' ]] && warp-cli --accept-tos connect >/dev/null 2>&1
+				[[ $(ss -nltp) =~ 'warp-svc' ]] && CLIENT_PORT=$(ss -nltp | grep warp-svc | grep -oP '127.0*\S+' | cut -d: -f2) && STATUS[2]=1 || STATUS[2]=0
+			
+			else	if [[ ! $(ip a) =~ 'CloudflareWARP' ]]; then
 					warp-cli --accept-tos connect >/dev/null 2>&1
 					warp-cli --accept-tos enable-always-on >/dev/null 2>&1
 					sleep 5
@@ -192,9 +195,6 @@ check_warp(){
 					ip -4 rule add table main suppress_prefixlength 0
 				fi
 				[[ $(ip a) =~ 'CloudflareWARP' ]] && STATUS[2]=1 || STATUS[2]=0
-		
-			else [[ ! $(ss -nltp) =~ 'warp-svc' ]] && warp-cli --accept-tos connect >/dev/null 2>&1
-				[[ $(ss -nltp) =~ 'warp-svc' ]] && CLIENT_PORT=$(ss -nltp | grep warp-svc | grep -oP '127.0*\S+' | cut -d: -f2) && STATUS[2]=1 || STATUS[2]=0
 			fi
 		else STATUS[2]=0
 		fi
@@ -210,7 +210,7 @@ check_warp(){
 
 	CASE_IPV4(){ NIC='-ks4m8'; RESTART="wgcf_restart"; }
 	CASE_IPV6(){ NIC='-ks6m8'; RESTART="wgcf_restart"; }
-	CASE_CLIENT(){ NIC="-sx socks5h://localhost:$CLIENT_PORT" && RESTART="socks5_restart" && [[ -e /etc/wireguard/luban ]] && NIC='--interface CloudflareWARP' && RESTART="interface_restart"; }
+	CASE_CLIENT(){ NIC='-s4m8 --interface CloudflareWARP' && RESTART="interface_restart" && [[ $(warp-cli --accept-tos settings) =~ WarpProxy ]] && NIC="-sx socks5h://localhost:$CLIENT_PORT" && RESTART="socks5_restart"; }
 	CASE_WIREPROXY(){ NIC="-sx socks5h://localhost:$WIREPROXY_PORT"; RESTART="wireproxy_restart"; }
 
 	INSTALL_CHECK=("0 0 0 0" "1 1 1 1" "0 1 1 1" "1 0 1 1" "1 1 0 1" "1 1 1 0" "0 0 1 1" "0 1 0 1" "0 1 1 0" "1 0 0 1" "1 0 1 0" "1 1 0 0" "0 0 0 1"  "0 0 1 0" "0 1 0 0" "1 0 0 0")
