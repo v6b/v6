b68fe28e7378806ccf6da5581cb2ae7eef6e823a
v6b <99478294+v6b@users.noreply.github.com>
2022-05-10 17:05:51 +0000

    Update

--- realm/src/consts.rs
+++ realm/src/consts.rs
@@ -20,26 +20,32 @@ macro_rules! def_feat {
     };
 }
 
+def_feat!(FEATURE_HOOK, "hook");
 def_feat!(FEATURE_PROXY, "proxy");
 def_feat!(FEATURE_MIMALLOC, "mimalloc");
 def_feat!(FEATURE_JEMALLOC, "jemalloc");
 def_feat!(FEATURE_MULTI_THREAD, "multi-thread");
 def_feat!(FEATURE_TRANSPORT, "transport");
+def_feat!(FEATURE_BRUTAL_SHUTDOWN, "brutal-shutdown");
 
 pub struct Features {
     pub mimalloc: bool,
     pub jemalloc: bool,
     pub multi_thread: bool,
+    pub hook: bool,
     pub proxy: bool,
     pub transport: bool,
+    pub brutal_shutdown: bool,
 }
 
 pub const FEATURES: Features = Features {
     mimalloc: FEATURE_MIMALLOC,
     jemalloc: FEATURE_JEMALLOC,
     multi_thread: FEATURE_MULTI_THREAD,
+    hook: FEATURE_HOOK,
     proxy: FEATURE_PROXY,
     transport: FEATURE_TRANSPORT,
+    brutal_shutdown: FEATURE_BRUTAL_SHUTDOWN,
 };
 
 impl Display for Features {
@@ -52,6 +58,8 @@ impl Display for Features {
             };
         }
 
+        disp_feat!(brutal_shutdown, "brutal");
+        disp_feat!(hook, "hook");
         disp_feat!(proxy, "proxy");
         disp_feat!(transport, "transport");
         disp_feat!(multi_thread, "multi-thread");
