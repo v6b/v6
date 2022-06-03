449e4dab317034143ae4ea76dddeea98854017c3
v6b <99478294+v6b@users.noreply.github.com>
2022-06-03 16:56:32 +0000

    Update

--- gallery-dl/gallery_dl/extractor/weibo.py
+++ gallery-dl/gallery_dl/extractor/weibo.py
@@ -15,6 +15,9 @@ import itertools
 import random
 import json
 
+BASE_PATTERN = r"(?:https?://)?(?:www\.|m\.)?weibo\.c(?:om|n)"
+USER_PATTERN = BASE_PATTERN + r"/(?:(u|n|p(?:rofile)?)/)?([^/?#]+)(?:/home)?"
+
 
 class WeiboExtractor(Extractor):
     category = "weibo"
@@ -26,6 +29,7 @@ class WeiboExtractor(Extractor):
 
     def __init__(self, match):
         Extractor.__init__(self, match)
+        self._prefix, self.user = match.groups()
         self.retweets = self.config("retweets", True)
         self.videos = self.config("videos", True)
         self.livephoto = self.config("livephoto", True)
@@ -37,9 +41,122 @@ class WeiboExtractor(Extractor):
     def request(self, url, **kwargs):
         response = Extractor.request(self, url, **kwargs)
 
-        if not response.history or "passport.weibo.com" not in response.url:
-            return response
+        if response.history and "passport.weibo.com" in response.url:
+            self._sina_visitor_system(response)
+            response = Extractor.request(self, url, **kwargs)
 
+        return response
+
+    def items(self):
+        original_retweets = (self.retweets == "original")
+
+        for status in self.statuses():
+
+            status["date"] = text.parse_datetime(
+                status["created_at"], "%a %b %d %H:%M:%S %z %Y")
+            yield Message.Directory, status
+
+            if self.retweets and "retweeted_status" in status:
+                if original_retweets:
+                    status = status["retweeted_status"]
+                    files = self._files_from_status(status)
+                else:
+                    files = itertools.chain(
+                        self._files_from_status(status),
+                        self._files_from_status(status["retweeted_status"]),
+                    )
+            else:
+                files = self._files_from_status(status)
+
+            for num, file in enumerate(files, 1):
+                if file["url"].startswith("http:"):
+                    file["url"] = "https:" + file["url"][5:]
+                if "filename" not in file:
+                    text.nameext_from_url(file["url"], file)
+                file["status"] = status
+                file["num"] = num
+                yield Message.Url, file["url"], file
+
+    def _files_from_status(self, status):
+        pic_ids = status.get("pic_ids")
+        if pic_ids:
+            pics = status["pic_infos"]
+            for pic_id in pic_ids:
+                pic = pics[pic_id]
+                pic_type = pic.get("type")
+
+                if pic_type == "gif" and self.videos:
+                    yield {"url": pic["video"]}
+
+                elif pic_type == "livephoto" and self.livephoto:
+                    yield pic["largest"].copy()
+
+                    file = {"url": pic["video"]}
+                    file["filehame"], _, file["extension"] = \
+                        pic["video"].rpartition("%2F")[2].rpartition(".")
+                    yield file
+
+                else:
+                    yield pic["largest"].copy()
+
+        if "page_info" in status:
+            page_info = status["page_info"]
+            if "media_info" not in page_info or not self.videos:
+                return
+            media = max(page_info["media_info"]["playback_list"],
+                        key=lambda m: m["meta"]["quality_index"])
+            yield media["play_info"].copy()
+
+    def _status_by_id(self, status_id):
+        url = "{}/ajax/statuses/show?id={}".format(self.root, status_id)
+        return self.request(url).json()
+
+    def _user_id(self):
+        if self.user.isdecimal():
+            return self.user[-10:]
+        else:
+            url = "{}/ajax/profile/info?{}={}".format(
+                self.root,
+                "screen_name" if self._prefix == "n" else "custom",
+                self.user)
+            return self.request(url).json()["data"]["user"]["idstr"]
+
+    def _pagination(self, endpoint, params):
+        url = self.root + "/ajax" + endpoint
+        headers = {
+            "X-Requested-With": "XMLHttpRequest",
+            "X-XSRF-TOKEN": None,
+            "Referer": "{}/u/{}".format(self.root, params["uid"]),
+        }
+
+        while True:
+            response = self.request(url, params=params, headers=headers)
+            headers["Accept"] = "application/json, text/plain, */*"
+            headers["X-XSRF-TOKEN"] = response.cookies.get("XSRF-TOKEN")
+
+            data = response.json()
+            if not data.get("ok"):
+                self.log.debug(response.content)
+                if "since_id" not in params:  # first iteration
+                    raise exception.StopExtraction(
+                        '"%s"', data.get("msg") or "unknown error")
+
+            data = data["data"]
+            statuses = data["list"]
+            if not statuses:
+                return
+            yield from statuses
+
+            if "next_cursor" in data:
+                params["cursor"] = data["next_cursor"]
+            elif "page" in params:
+                params["page"] += 1
+            elif data["since_id"]:
+                params["sinceid"] = data["since_id"]
+            else:
+                params["since_id"] = statuses[-1]["id"] - 1
+
+    def _sina_visitor_system(self, response):
         self.log.info("Sina Visitor System")
 
         passport_url = "https://passport.weibo.com/visitor/genvisitor"
@@ -66,142 +183,132 @@ class WeiboExtractor(Extractor):
             "_rand": random.random(),
         }
         response = Extractor.request(self, passport_url, params=params)
-
         _cookie_cache.update("", response.cookies)
-
-        return Extractor.request(self, url, **kwargs)
-
-    def items(self):
-        original_retweets = (self.retweets == "original")
-
-        for status in self.statuses():
-
-            status["date"] = text.parse_datetime(
-                status["created_at"], "%a %b %d %H:%M:%S %z %Y")
-            yield Message.Directory, status
-
-            if self.retweets and "retweeted_status" in status:
-                if original_retweets:
-                    status = status["retweeted_status"]
-                    files = self._files_from_status(status)
-                else:
-                    files = itertools.chain(
-                        self._files_from_status(status),
-                        self._files_from_status(status["retweeted_status"]),
-                    )
-            else:
-                files = self._files_from_status(status)
-
-            for num, file in enumerate(files, 1):
-                if "filename" not in file:
-                    text.nameext_from_url(file["url"], file)
-                file["status"] = status
-                file["num"] = num
-                yield Message.Url, file["url"], file
-
-    def _status_by_id(self, status_id):
-        url = "{}/ajax/statuses/show?id={}".format(self.root, status_id)
-        return self.request(url).json()
-
-    def _files_from_status(self, status):
-        pic_ids = status.get("pic_ids")
-        if pic_ids:
-            pics = status["pic_infos"]
-            for pic_id in pic_ids:
-                pic = pics[pic_id]
-                yield pic["largest"].copy()
-
-                if pic.get("type") == "livephoto" and self.livephoto:
-                    file = {"url": pic["video"]}
-                    file["filehame"], _, file["extension"] = \
-                        pic["video"].rpartition("%2F")[2].rpartition(".")
-                    yield file
-
-        if "page_info" in status:
-            page_info = status["page_info"]
-            if "media_info" not in page_info or not self.videos:
-                return
-            media = max(page_info["media_info"]["playback_list"],
-                        key=lambda m: m["meta"]["quality_index"])
-            yield media["play_info"].copy()
 
 
 class WeiboUserExtractor(WeiboExtractor):
-    """Extractor for all images of a user on weibo.cn"""
+    """Extractor for weibo user profiles"""
     subcategory = "user"
-    pattern = (r"(?:https?://)?(?:www\.|m\.)?weibo\.c(?:om|n)"
-               r"/(?:(u|n|p(?:rofile)?)/)?([^/?#]+)(?:/home)?/?(?:$|\?|#)")
+    pattern = USER_PATTERN + r"(?:$|#)"
     test = (
-        ("https://m.weibo.cn/u/2314621010", {
-            "range": "1-20",
-        }),
-        ("https://weibo.com/zhouyuxi77", {
-            "keyword": {"status": {"user": {"id": 7488709788}}},
-            "range": "1",
-        }),
-        ("https://www.weibo.com/n/周于希Sally", {
-            "keyword": {"status": {"user": {"id": 7488709788}}},
-            "range": "1",
-        }),
-        # deleted (#2521)
-        ("https://weibo.com/u/7500315942", {
-            "count": 0,
-        }),
+        ("https://weibo.com/1758989602"),
+        ("https://weibo.com/u/1758989602"),
+        ("https://weibo.com/p/1758989602"),
         ("https://m.weibo.cn/profile/2314621010"),
         ("https://m.weibo.cn/p/2304132314621010_-_WEIBO_SECOND_PROFILE_WEIBO"),
         ("https://www.weibo.com/p/1003062314621010/home"),
     )
 
-    def __init__(self, match):
-        WeiboExtractor.__init__(self, match)
-        self.type, self.user = match.groups()
+    def items(self):
+        base = " {}/u/{}?tabtype=".format(self.root, self._user_id())
+        return self._dispatch_extractors((
+            (WeiboHomeExtractor  , base + "home"),
+            (WeiboFeedExtractor  , base + "feed"),
+            (WeiboVideosExtractor, base + "newVideo"),
+            (WeiboAlbumExtractor , base + "album"),
+        ), ("feed",))
+
+
+class WeiboHomeExtractor(WeiboExtractor):
+    """Extractor for weibo 'home' listings"""
+    subcategory = "home"
+    pattern = USER_PATTERN + r"\?tabtype=home"
+    test = ("https://weibo.com/1758989602?tabtype=home", {
+        "range": "1-30",
+        "count": 30,
+    })
 
     def statuses(self):
-        if self.user.isdecimal():
-            user_id = self.user[-10:]
-        else:
-            url = "{}/ajax/profile/info?{}={}".format(
-                self.root,
-                "screen_name" if self.type == "n" else "custom",
-                self.user)
-            user_id = self.request(url).json()["data"]["user"]["idstr"]
+        endpoint = "/profile/myhot"
+        params = {"uid": self._user_id(), "page": 1, "feature": "2"}
+        return self._pagination(endpoint, params)
 
-        url = self.root + "/ajax/statuses/mymblog"
-        params = {
-            "uid": user_id,
-            "feature": "0",
-        }
-        headers = {
-            "X-Requested-With": "XMLHttpRequest",
-            "X-XSRF-TOKEN": None,
-            "Referer": "{}/u/{}".format(self.root, user_id),
-        }
 
-        while True:
-            response = self.request(url, params=params, headers=headers)
-            headers["Accept"] = "application/json, text/plain, */*"
-            headers["X-XSRF-TOKEN"] = response.cookies.get("XSRF-TOKEN")
+class WeiboFeedExtractor(WeiboExtractor):
+    """Extractor for weibo user feeds"""
+    subcategory = "feed"
+    pattern = USER_PATTERN + r"\?tabtype=feed"
+    test = (
+        ("https://weibo.com/1758989602?tabtype=feed", {
+            "range": "1-30",
+            "count": 30,
+        }),
+        ("https://weibo.com/zhouyuxi77?tabtype=feed", {
+            "keyword": {"status": {"user": {"id": 7488709788}}},
+            "range": "1",
+        }),
+        ("https://www.weibo.com/n/周于希Sally?tabtype=feed", {
+            "keyword": {"status": {"user": {"id": 7488709788}}},
+            "range": "1",
+        }),
+        # deleted (#2521)
+        ("https://weibo.com/u/7500315942?tabtype=feed", {
+            "count": 0,
+        }),
+    )
 
-            data = response.json()
-            if not data.get("ok"):
-                self.log.debug(response.content)
-                if "since_id" not in params:  # first iteration
-                    raise exception.StopExtraction(
-                        '"%s"', data.get("msg") or "unknown error")
+    def statuses(self):
+        endpoint = "/statuses/mymblog"
+        params = {"uid": self._user_id(), "feature": "0"}
+        return self._pagination(endpoint, params)
 
-            statuses = data["data"]["list"]
-            if not statuses:
-                return
-            yield from statuses
 
-            params["since_id"] = statuses[-1]["id"] - 1
+class WeiboVideosExtractor(WeiboExtractor):
+    """Extractor for weibo 'newVideo' listings"""
+    subcategory = "videos"
+    pattern = USER_PATTERN + r"\?tabtype=newVideo"
+    test = ("https://weibo.com/1758989602?tabtype=newVideo", {
+        "pattern": r"https://f\.video\.weibocdn\.com/(../)?\w+\.mp4\?label=mp",
+        "range": "1-30",
+        "count": 30,
+    })
+
+    def statuses(self):
+        endpoint = "/profile/getWaterFallContent"
+        params = {"uid": self._user_id()}
+        return self._pagination(endpoint, params)
+
+
+class WeiboArticleExtractor(WeiboExtractor):
+    """Extractor for weibo 'article' listings"""
+    subcategory = "article"
+    pattern = USER_PATTERN + r"\?tabtype=article"
+    test = ("https://weibo.com/1758989602?tabtype=article", {
+        "count": 0,
+    })
+
+    def statuses(self):
+        endpoint = "/statuses/mymblog"
+        params = {"uid": self._user_id(), "page": 1, "feature": "10"}
+        return self._pagination(endpoint, params)
+
+
+class WeiboAlbumExtractor(WeiboExtractor):
+    """Extractor for weibo 'album' listings"""
+    subcategory = "album"
+    pattern = USER_PATTERN + r"\?tabtype=album"
+    test = ("https://weibo.com/1758989602?tabtype=album", {
+        "pattern": r"https://wx\d+\.sinaimg\.cn/large/\w{32}\.(jpg|png|gif)",
+        "range": "1-3",
+        "count": 3,
+    })
+
+    def statuses(self):
+        endpoint = "/profile/getImageWall"
+        params = {"uid": self._user_id()}
+
+        seen = set()
+        for image in self._pagination(endpoint, params):
+            mid = image["mid"]
+            if mid not in seen:
+                seen.add(mid)
+                yield self._status_by_id(mid)
 
 
 class WeiboStatusExtractor(WeiboExtractor):
     """Extractor for images from a status on weibo.cn"""
     subcategory = "status"
-    pattern = (r"(?:https?://)?(?:www\.|m\.)?weibo\.c(?:om|n)"
-               r"/(?:detail|status|\d+)/(\w+)")
+    pattern = BASE_PATTERN + r"/(detail|status|\d+)/(\w+)"
     test = (
         ("https://m.weibo.cn/detail/4323047042991618", {
             "pattern": r"https?://wx\d+.sinaimg.cn/large/\w+.jpg",
@@ -221,22 +328,23 @@ class WeiboStatusExtractor(WeiboExtractor):
             "options": (("retweets", "original"),),
             "keyword": {"status": {"id": 4600167083287033}},
         }),
-        # livephoto (#2146)
+        # type == livephoto (#2146)
         ("https://weibo.com/5643044717/KkuDZ4jAA", {
             "range": "2,4,6",
             "pattern": r"https://video\.weibo\.com/media/play\?livephoto="
                        r"https%3A%2F%2Fus.sinaimg.cn%2F\w+\.mov",
         }),
+        # type == gif
+        ("https://weibo.com/1758989602/LvBhm5DiP", {
+            "pattern": r"http://g\.us\.sinaimg.cn/o0/qNZcaAAglx07Wuf921CM01041"
+                       r"20005tc0E010\.mp4\?label=gif_mp4",
+        }),
         ("https://m.weibo.cn/status/4339748116375525"),
         ("https://m.weibo.cn/5746766133/4339748116375525"),
     )
 
-    def __init__(self, match):
-        WeiboExtractor.__init__(self, match)
-        self.status_id = match.group(1)
-
     def statuses(self):
-        return (self._status_by_id(self.status_id),)
+        return (self._status_by_id(self.user),)
 
 
 @cache(maxage=356*86400)
