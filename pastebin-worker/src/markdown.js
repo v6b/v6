5b0c9cef32447e106a4666b6754f66ea4f5762f9
v6b <99478294+v6b@users.noreply.github.com>
2022-06-28 10:44:02 +0000

    Update

---
+++ pastebin-worker/src/markdown.js
@@ -0,0 +1,37 @@
+import {unified} from 'unified'
+import remarkParse from 'remark-parse'
+import remarkGfm from 'remark-gfm'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
+
+export async function makeMarkdown(content) {
+  const convertedHtml = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process(content)
+
+  return `<!DOCTYPE html>
+<html lang='en'>
+<head>
+  <title>Yet another pastebin</title>
+  <meta charset='utf-8'>
+  <link href='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css' rel='stylesheet' />
+  <link href='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/line-numbers/prism-line-numbers.css' rel='stylesheet' />
+  <link rel='stylesheet' href='https://pages.github.com/assets/css/style.css'>
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+  <script id="MathJax-script" async
+          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+  </script>
+</head>
+<body>
+<article class='line-numbers container-lg px-3 my-5 markdown-body'>
+${convertedHtml}
+</article>
+  <script src='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/components/prism-core.min.js'></script>
+  <script src='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/line-numbers/prism-line-numbers.min.js'></script>
+  <script src='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/autoloader/prism-autoloader.min.js'></script>
+</html>
+`
+}
