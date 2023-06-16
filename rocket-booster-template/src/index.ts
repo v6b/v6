3329a0f91cc43ad1355c960b83be962f6f9be4b0
v6b <99478294+v6b@users.noreply.github.com>
2023-06-16 01:58:47 +0000

    Update

--- rocket-booster-template/src/index.ts
+++ rocket-booster-template/src/index.ts
@@ -9,7 +9,7 @@ export default {
     reflare.push({
       path: '/*',
       upstream: {
-        domain: 'httpbin.org',
+        domain: 'httpbingo.org',
         protocol: 'https',
       },
     });
