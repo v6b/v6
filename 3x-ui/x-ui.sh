fe561f9ec77af20a98b6e98a4b4c18e4af469e2c
v6b <99478294+v6b@users.noreply.github.com>
2024-01-20 16:36:58 +0000

    Update

--- 3x-ui/x-ui.sh
+++ 3x-ui/x-ui.sh
@@ -869,34 +869,32 @@ run_speedtest() {
 }
 
 create_iplimit_jails() {
-    # Set default bantime to 30 minutes if not provided
+    # Use default bantime if not passed => 30 minutes
     local bantime="${1:-30}"
 
-    # Uncomment 'allowipv6 = auto' in fail2ban.conf
+    # uncomment allowipv6 = auto in fail2ban.conf
     sed -i 's/#allowipv6 = auto/allowipv6 = auto/g' /etc/fail2ban/fail2ban.conf
 
-    # Create 3x-ipl jail configuration
-    jail_config="[3x-ipl]
+    cat << EOF > /etc/fail2ban/jail.d/3x-ipl.conf
+[3x-ipl]
 enabled=true
 filter=3x-ipl
 action=3x-ipl
 logpath=${iplimit_log_path}
 maxretry=4
 findtime=60
-bantime=${bantime}m"
+bantime=${bantime}m
+EOF
 
-    echo "$jail_config" >/etc/fail2ban/jail.d/3x-ipl.conf
-
-    # Create 3x-ipl filter definition
-    filter_definition="[Definition]
+    cat << EOF > /etc/fail2ban/filter.d/3x-ipl.conf
+[Definition]
 datepattern = ^%%Y/%%m/%%d %%H:%%M:%%S
 failregex   = \[LIMIT_IP\]\s*Email\s*=\s*<F-USER>.+</F-USER>\s*\|\|\s*SRC\s*=\s*<ADDR>
-ignoreregex ="
+ignoreregex =
+EOF
 
-    echo "$filter_definition" >/etc/fail2ban/filter.d/3x-ipl.conf
-
-    # Create 3x-ipl action definition
-    action_definition="[INCLUDES]
+    cat << EOF > /etc/fail2ban/action.d/3x-ipl.conf
+[INCLUDES]
 before = iptables-common.conf
 
 [Definition]
@@ -911,16 +909,15 @@ actionstop = <iptables> -D <chain> -p <protocol> -j f2b-<name>
 actioncheck = <iptables> -n -L <chain> | grep -q 'f2b-<name>[ \t]'
 
 actionban = <iptables> -I f2b-<name> 1 -s <ip> -j <blocktype>
-            echo \"\$(date +\"%%Y/%%m/%%d %%H:%%M:%%S\")   BAN   [Email] = <F-USER> [IP] = <ip> banned for <bantime> seconds.\" >> ${iplimit_banned_log_path}
+            echo "\$(date +"%%Y/%%m/%%d %%H:%%M:%%S")   BAN   [Email] = <F-USER> [IP] = <ip> banned for <bantime> seconds." >> ${iplimit_banned_log_path}
 
 actionunban = <iptables> -D f2b-<name> -s <ip> -j <blocktype>
-              echo \"\$(date +\"%%Y/%%m/%%d %%H:%%M:%%S\")   UNBAN   [Email] = <F-USER> [IP] = <ip> unbanned.\" >> ${iplimit_banned_log_path}
+              echo "\$(date +"%%Y/%%m/%%d %%H:%%M:%%S")   UNBAN   [Email] = <F-USER> [IP] = <ip> unbanned." >> ${iplimit_banned_log_path}
 
-[Init]"
+[Init]
+EOF
 
-    echo "$action_definition" >/etc/fail2ban/action.d/3x-ipl.conf
-
-    echo -e "${green}Ip Limit jail files created with a bantime of ${bantime} minutes.${plain}"
+    echo -e "${green}Created Ip Limit jail files with a bantime of ${bantime} minutes.${plain}"
 }
 
 iplimit_remove_conflicts() {
@@ -960,7 +957,7 @@ iplimit_main() {
         fi
         ;;
     2)
-        read -rp "Please enter new Ban Duration in Minutes [default 5]: " NUM
+        read -rp "Please enter new Ban Duration in Minutes [default 30]: " NUM
         if [[ $NUM =~ ^[0-9]+$ ]]; then
             create_iplimit_jails ${NUM}
             systemctl restart fail2ban
